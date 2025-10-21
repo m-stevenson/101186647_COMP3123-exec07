199fa039f0c6ca9ce430ffc50fa9dc447cd26a80
m-stevenson <mitchell.stevenson@georgebrown.ca>
2025-10-20 23:53:27 -0400

    Lab Week 7

---
+++ labweek09/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome Fullstack Development - I</h1>
+        <h3>React JS Programming Week09 Lab Exercise</h3>
+        <p>101186647</p> 
+        <p>Mitchell Stevenson</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
